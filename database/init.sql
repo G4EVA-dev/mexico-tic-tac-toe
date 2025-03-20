4f745dd66c7ed3c93613548276edaa5af2ce9b4f
G4EVA-dev <tanzeglenn@gmail.com>
2025-03-20 11:25:09 +0100

    feat: update database

--- database/init.sql
+++ database/init.sql
@@ -13,3 +13,14 @@ CREATE INDEX idx_game_rooms_status ON game_rooms(status);
 
 INSERT INTO game_rooms (player1, board, current_turn, status) VALUES
 ('Player1', '---------', 'Player1', 'waiting');
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    game_id UUID NOT NULL UNIQUE,
+    player1 VARCHAR(255) NOT NULL,
+    player2 VARCHAR(255),
+    board TEXT NOT NULL, -- Store the board as a JSON string
+    current_turn VARCHAR(255),
+    status VARCHAR(50) NOT NULL,
+    winner VARCHAR(255)
+);
